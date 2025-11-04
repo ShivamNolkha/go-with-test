4203a375fc42de2dc17e08f70a763ddeb440f5fe
shivam nolkha <shivamnolkha@gmail.com>
2025-11-05 01:03:55 +0530

    feat: Completed helloworld package

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -1,17 +1,27 @@
-package main
+package helloworld
 
 import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorretMessage(t, got, want)
 
 	})
 	t.Run("say 'Hello, World' when an empty string in supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorretMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Shivam", "Spanish")
+		want := "Hola, Shivam"
+		assertCorretMessage(t, got, want)
+	})
+	t.Run("in hindi", func(t *testing.T){
+		got := Hello("Shivam", "Hindi")
+		want := "Namaste, Shivam"
 		assertCorretMessage(t, got, want)
 	})
 }
