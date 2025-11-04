ba1d301ccd7359f430e97408605b7d8aa8ea0956
shivam nolkha <shivamnolkha@gmail.com>
2025-11-05 00:45:29 +0530

    feat: will greet properly even if we pass empty value

---
+++ hello_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorretMessage(t, got, want)
+
+	})
+	t.Run("say 'Hello, World' when an empty string in supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorretMessage(t, got, want)
+	})
+}
+
+func assertCorretMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
