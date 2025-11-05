428f523f1471e734de6651a8f9a26483c81c415b
shivam nolkha <shivamnolkha@gmail.com>
2025-11-05 18:04:08 +0530

    Learned more about slices and variadic arguments

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,8 +1,19 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
 	return
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	fmt.Println(numbersToSum)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
 }
