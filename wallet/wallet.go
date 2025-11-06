a838a285ab511bf13f318e048754b9e13fdd29a2
shivam nolkha <shivamnolkha@gmail.com>
2025-11-06 14:05:59 +0530

    Learned about errors and pointers and defining custom types

---
+++ wallet/wallet.go
@@ -0,0 +1,39 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// fmt.Printf("address of balance in test is %p \n", &w.balance)
+	w.balance += amount
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
