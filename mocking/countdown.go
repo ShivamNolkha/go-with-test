2bf8841a90de35615a958298ee7e628727c6719b
shivam nolkha <shivamnolkha@gmail.com>
2025-11-08 00:20:19 +0530

    tried mock time.Sleep() for testing

---
+++ mocking/countdown.go
@@ -0,0 +1,36 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countDownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func CountDown(out io.Writer, sleeper Sleeper) {
+	for i := countDownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,26 @@
+package mocking
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountDown(t *testing.T) {
+	buffer := bytes.Buffer{}
+	spySleeper := SpySleeper{}
+
+	CountDown(&buffer, &spySleeper)
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
