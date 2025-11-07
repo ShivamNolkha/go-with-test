b6c0ed95afd03b9467f02649161e3a983f66a24b
shivam nolkha <shivamnolkha@gmail.com>
2025-11-08 00:46:08 +0530

    added configurable sleep timeout

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,25 +2,85 @@ package mocking
 
 import (
 	"bytes"
+	"slices"
 	"testing"
+	"time"
 )
 
 func TestCountDown(t *testing.T) {
-	buffer := bytes.Buffer{}
-	spySleeper := SpySleeper{}
-
-	CountDown(&buffer, &spySleeper)
-	got := buffer.String()
-	want := `3
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+		CountDown(&buffer, &SpyCountDownOperations{})
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountDownOperations{}
+		CountDown(spySleepPrinter, spySleepPrinter)
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !slices.Equal(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+	spyTime := SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.SetDurationSlept}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type SpyCountDownOperations struct {
+	Calls []string
+}
+
+const sleep = "sleep"
+const write = "write"
+
+func (s *SpyCountDownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+
+}
+
+func (s *SpyCountDownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
 }
