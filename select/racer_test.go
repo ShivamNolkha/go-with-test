a85e04297dff5448732f531547346c6e6e5838d5
shivam nolkha <shivamnolkha@gmail.com>
2025-11-08 12:03:57 +0530

    learned about httptest package

---
+++ select/racer_test.go
@@ -0,0 +1,36 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowUrl := slowServer.URL
+	// fmt.Println(slowUrl)
+	fastUrl := fastServer.URL
+	want := fastUrl
+
+	got := Racer(slowUrl, fastUrl)
+
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
